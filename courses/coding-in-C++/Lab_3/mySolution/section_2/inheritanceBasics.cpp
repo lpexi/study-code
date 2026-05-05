7145961c9730a7e86202953bcaa9e83fe3f9bf9e
lpexi <253278427+lpexi@users.noreply.github.com>
2026-05-05 22:53:32 +0200

    Added comments

--- courses/coding-in-C++/Lab_3/mySolution/section_2/inheritanceBasics.cpp
+++ courses/coding-in-C++/Lab_3/mySolution/section_2/inheritanceBasics.cpp
@@ -1,7 +1,25 @@
+/**
+ * @file inheritanceBasics.cpp
+ * @author @lpexi
+ * @brief This file contains the solution to Lab 3 section 2. A User class is created and from this
+ * class, multible derived classes with inheritance are created to train basic inheritance in C++.
+ * To see whats happening, the info stored in the Sturcts is printed to the terminal
+ * @version 0.1
+ * @date 2026-05-05
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
 #include <iostream>
 #include <cstdint>
 #include <string>
 
+/*
+ Only protected member can be inherited by the derived classes and at the same time not be used
+ outside of the class. If one would use private, the attributes would not be visible ore accessable
+ in the derived classes.
+*/
 class User
 {
 private:
@@ -11,8 +29,17 @@ protected:
     std::uint_fast16_t id;
 
 public:
+    /**
+     * @brief Construct a new User object
+     * 
+     * @param name  Name of the user
+     * @param id    ID of the user 
+     */
     User(std::string name, std::uint_fast16_t id);
-    
+    /**
+     * @brief Prints the stored information within the class User
+     * Prints the name and ID to the terminal
+     */
     void printInfo();
 };
 
@@ -34,9 +61,19 @@ private:
     std::uint_fast32_t matrNr;
 
 public:
-
+    /**
+     * @brief Construct a new Student object
+     * 
+     * @param name      Name of the student
+     * @param id        Student ID
+     * @param matrNr    Matrikel number of student
+     */
     Student(std::string name, std::uint_fast16_t id, std::uint_fast32_t matrNr);
 
+    /**
+     * @brief Prints the info held within the Student class
+     * Prints the Name, Student ID and Matrikel number
+     */
     void printRole();
 };
 
@@ -59,8 +96,19 @@ private:
     std::uint_fast16_t employeeNr;
 
 public:
+    /**
+     * @brief Construct a new Instructor object
+     * 
+     * @param name          Name of Instructor
+     * @param id            ID of instructor
+     * @param employeeNr    Emloyee number of Instructor 
+     */
     Instructor(std::string name, std::uint_fast16_t id, std::uint_fast16_t employeeNr);
 
+    /**
+     * @brief Prints the info held within the Instructor class
+     * Prints the Name, Instructor ID and Employee number
+     */
     void printRole();
 };
 
